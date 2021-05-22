02d6362dc913bc41f7aab48df323d441863c2ead
Taewon Kim <rlaxodnjs199@gmail.com>
2021-05-21 21:26:24 -0500

    update

--- copy_data.py
+++ copy_data.py
@@ -1,5 +1,17 @@
 import numpy
 
+# memory: 3a
+
+
+def normalize(array: numpy.ndarray):
+    low = array.min()
+    high = array.max()
+
+    return (array - low) / (high - low)
+    # memory: (a) -> original array, (a) -> array - low, (a) -> return array
+
+# memory: a: in-place replacement
+
 
 def normalize(array: numpy.ndarray):
     low = array.min()
@@ -8,3 +20,17 @@ def normalize(array: numpy.ndarray):
     array /= high
 
     return array
+    # memory: (a) -> original array
+
+# memory: 2a: hidden mutation
+
+
+def normalize3(array: numpy.ndarray):
+    low = array.min()
+    high = array.max()
+    result = array.copy()
+    result -= low
+    result /= (high-low)
+
+    return result
+    # memory: (a) -> original array (a) -> result array
