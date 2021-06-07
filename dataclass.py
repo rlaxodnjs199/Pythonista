2d04e0e4f4389c3ee3c2ed7d96562a586f7a7897
Taewon Kim <rlaxodnjs199@gmail.com>
2021-06-06 22:27:41 -0500

    investigate dataclass

--- dataclass.py
+++ dataclass.py
@@ -7,7 +7,7 @@ import math
 
 
 @dataclass
-class DataClass(object):
+class DataClass:
     # hidden attribute
     attr_0: InitVar[int] = 81
     # initialized attribute
@@ -39,8 +39,40 @@ class DataClass(object):
         )
 
 
+@dataclass
+class Inventory:
+    name: str
+    price: int
+    quantity: int = 0
+
+    # with @dataclass, below will be added automatically
+    # def __init__(self, name: str, unit_price: int, quantity: int = 0):
+    #   self.name = name
+    #   self.unit_price = unit_price
+    #   self.quantity = quantity
+
+    @classmethod
+    def create(cls):
+        return cls(name='twyn', price='100')
+
+    @classmethod
+    def access(cls, j):
+        print(j.name)
+
+    def total_cost(self) -> int:
+        return self.price * self.quantity
+
+    @staticmethod
+    def isExist(quantity) -> bool:
+        return quantity > 0
+
+
 if __name__ == '__main__':
     objects = [DataClass.create() for _ in range(100)]
     df = pd.DataFrame(objects)
-
     print(df)
+    i = Inventory(name='yunakim', price=30)
+    print(i.total_cost())
+    j = Inventory.create()
+    Inventory.access(j)
+    print(Inventory.isExist(j.quantity))
