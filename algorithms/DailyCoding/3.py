09d06e9eb9246797640bca002863877773100df7
Taewon Kim <rlaxodnjs199@gmail.com>
2021-08-09 20:03:56 -0500

    update

---
+++ algorithms/DailyCoding/3.py
@@ -0,0 +1,42 @@
+class Node:
+	def __init__(self, val, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+# def serialize(node: Node, s=""):
+# 	if not isinstance(node, Node):
+# 		return "# " 
+# 	s += (str(node.val) + " ")
+# 	s += serialize(node.left)
+# 	s += serialize(node.right)
+# 	return s
+
+# def deserialize(node_str: str, result=None):
+# 	if node_str.split()[0] == "#":
+# 		return None
+# 	result = Node(node_str.split()[0])
+# 	result.left = deserialize(node_str.partition(" ")[2], result)
+# 	result.right = deserialize(node_str.partition(" ")[2].partition(" ")[2], result)
+# 	return result
+
+def serialize(node: Node):
+	if not isinstance(node, Node):
+		return "#"
+	return f'{node.val} {serialize(node.left)} {serialize(node.right)}'
+
+def deserialize(node_str):
+	vals = iter(node_str.split())
+	def build_node():
+		val = next(vals)
+		if val == "#":
+			return None
+		node = Node(val)
+		node.left = build_node()
+		node.right = build_node()
+		return node
+	return build_node()
+
+if __name__ == "__main__":
+	node = Node('root', Node('left', Node('left.left')), Node('right'))
+	assert deserialize(serialize(node)).left.left.val == 'left.left'
